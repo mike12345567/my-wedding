ac46bbf334dcf49b39d1fa98cf6a33d6fb9ea43c
mike12345567 <me@michaeldrury.co.uk>
2021-05-22 11:49:00 +0100

    Adding functionality to bring up postgres automatically.

---
+++ packages/server/scripts/start.js
@@ -0,0 +1,8 @@
+const compose = require("docker-compose")
+const { join } = require("path")
+
+compose.upAll({ cwd: join(__dirname), log: false }).then(() => {
+  console.log("Postgres stack up and running.")
+}).catch(err => {
+  console.error(`FAILED TO COMPOSE - ${err}`)
+})
---
+++ packages/server/scripts/stop.js
@@ -0,0 +1,8 @@
+const compose = require("docker-compose")
+const { join } = require("path")
+
+compose.down({ cwd: join(__dirname), log: false }).then(() => {
+  console.log("Postgres stack stopped.")
+}).catch(err => {
+  console.error(`FAILED TO COMPOSE - ${err}`)
+})
