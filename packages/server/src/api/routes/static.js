e926223154d2a5f2bf730bab4acfd3db73b67f8f
mike12345567 <me@michaeldrury.co.uk>
2021-05-23 17:07:40 +0100

    Updating server routes.

--- packages/server/src/api/routes/static.js
+++ packages/server/src/api/routes/static.js
@@ -4,6 +4,7 @@ const router = Router()
 
 router
   .get("/", controller.serveApp)
+  // include the general path
   .get("/site/:path(.*)", controller.serveApp)
   .get("/assets/:path(.*)", controller.serveAssets)
   .get("/build/:path(.*)", controller.serveBuild)
